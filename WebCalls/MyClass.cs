9c3f791ef2b5452e5d102bb9887473403fd9d5ab
Richard Goforth <rgoforth@ceiamerica.com>
2014-07-03 08:59:15 -0400

    replace with modern http

--- WebCalls/MyClass.cs
+++ WebCalls/MyClass.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.IO;
-using System.Net;
 using System.Diagnostics;
+using System.Net.Http;
+using ModernHttpClient;
 
 namespace WebCalls
 {
@@ -18,9 +19,10 @@ namespace WebCalls
         public async Task<Stream> GetFileAsStream()
         {
             Debug.WriteLine("Get File As Stream " + BaseURL);
-            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+            HttpClient client = new HttpClient(new NativeMessageHandler());
             Debug.WriteLine("Begin Get Response");
             var response = await client.GetStreamAsync(BaseURL);
+
             Debug.WriteLine("End Get Response");
 
             return response;
